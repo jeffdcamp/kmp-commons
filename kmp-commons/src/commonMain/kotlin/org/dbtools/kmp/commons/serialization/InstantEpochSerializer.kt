c7a64e9df77cc696b2a7a76ab88d5d7f0c1b7c53
Jeff Campbell <jeffdcamp@gmail.com>
2025-12-16 13:43:31 -0700

    Suppressed warning with InstantEpochSerializer

--- kmp-commons/src/commonMain/kotlin/org/dbtools/kmp/commons/serialization/InstantEpochSerializer.kt
+++ kmp-commons/src/commonMain/kotlin/org/dbtools/kmp/commons/serialization/InstantEpochSerializer.kt
@@ -9,6 +9,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlin.time.Instant
 
+@Suppress("EXTERNAL_SERIALIZER_USELESS") // This is a serializer to convert Instant to an epoch Long (in millis)
 @Serializer(forClass = Instant::class)
 object InstantEpochSerializer: KSerializer<Instant> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("InstantEpochSerializer", PrimitiveKind.LONG)
